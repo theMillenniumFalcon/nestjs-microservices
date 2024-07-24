4a6c7c37abbe84a83e192a8cd77a713180d54452
Nishank Priydarshi <nishankpr2002@gmail.com>
2024-07-25 00:14:54 +0530

    added communication

---
+++ communication/src/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './create-user.event';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  handleUserCreated(data: CreateUserEvent) {
+    console.log('handleUserCreated - COMMS', data);
+
+    // other features to add: emailing user, sms, etc.
+  }
+}
